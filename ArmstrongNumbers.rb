10a1c55c7df6526b2b1f6b0d8e4bc4f7e1c61a16
trodriguez30 <tatianarodriguezpacheco@gmail.com>
2018-04-25 11:51:28 -0400

    Coding challenge completed <SoloLearn>: Armstrong Numbers

---
+++ ArmstrongNumbers.rb
@@ -0,0 +1,14 @@
+def armstrongNumber(n)
+  acum=0
+  arr=n.to_s.split('').map(&:to_i)
+  dig=arr.length
+  arr.length.times do |x|
+    acum += arr[x] ** dig
+  end 
+  puts acum == n ? "#{n} is an Armstrong number" : "#{n} is not an Armstrong number"
+end 
+
+puts 'Enter a number:'
+num = gets.chomp.to_i
+
+armstrongNumber(num)
