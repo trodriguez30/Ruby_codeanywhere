3dd231e7d4a9c72395062243e15fe7de7db33096
trodriguez30 <tatianarodriguezpacheco@gmail.com>
2018-05-11 16:54:42 -0400

    < Nth prime modified>

--- Exercism.io/nth_prime/nth_prime.rb
+++ Exercism.io/nth_prime/nth_prime.rb
@@ -1,10 +1,26 @@
-require 'prime'
-
 # class Nth prime
 class Prime
+  @prime = []
+
   def self.nth(num)
     raise ArgumentError if num.zero?
-    nth = Prime.first num
-    nth.last
+    @prime.clear
+    x = 2
+    @prime << 2
+    while @prime.length < num
+      x += 1
+      next if (x % 2).zero?
+      add(x)
+    end
+    @prime.last
+  end
+
+  def self.add(num)
+    cont = 0
+    @prime.each do |i|
+      break if i > Math.sqrt(num)
+      cont += 1 if (num % i).zero?
+    end
+    @prime << num if cont.zero?
   end
 end
