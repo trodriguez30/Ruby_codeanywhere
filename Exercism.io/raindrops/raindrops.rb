1106306f38a81adb2a75310b515fc02eae10279b
trodriguez30 <tatianarodriguezpacheco@gmail.com>
2018-05-11 16:57:14 -0400

    <Raindrops optimized> factors method removed

--- Exercism.io/raindrops/raindrops.rb
+++ Exercism.io/raindrops/raindrops.rb
@@ -2,9 +2,9 @@
 class Raindrops
   def self.convert(num)
     raindrops = ''
-    raindrops += 'Pling' if num % 3 == 0
-    raindrops += 'Plang' if num % 5 == 0
-    raindrops += 'Plong' if num % 7 == 0
+    raindrops += 'Pling' if (num % 3).zero?
+    raindrops += 'Plang' if (num % 5).zero?
+    raindrops += 'Plong' if (num % 7).zero?
     raindrops = num.to_s if raindrops == ''
     raindrops
   end
